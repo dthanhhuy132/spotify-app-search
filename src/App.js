a85e7d906b9c7f29c73c5334b2906f176036ed9c
huydoan <dthanhhuy132@gmail.com>
2022-05-22 11:08:13 +0700

    why found 403

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
         <div className="login">
           <SpotifyAuth
             redirectUri="https://hd-spotify-app-search.vercel.app"
-            clientID="b64778bc8ee1462da13c211c2acb98a9"
+            clientID={process.env.REACT_APP_CLIENT_ID}
             scopes={[Scopes.userReadPrivate, "user-read-email"]}
             onAccessToken={(token) => setToken(token)}
           />
