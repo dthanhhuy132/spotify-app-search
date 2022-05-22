ab8677936c2f7c5158a41948b29ee6378b2a7e64
huydoan <dthanhhuy132@gmail.com>
2022-05-22 11:04:54 +0700

    change scopr

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
           <SpotifyAuth
             redirectUri="https://hd-spotify-app-search.vercel.app"
             clientID="b64778bc8ee1462da13c211c2acb98a9"
-            // scopes={[Scopes.userReadPrivate, "user-read-email"]}
+            scopes={[Scopes.userReadPrivate, "user-read-email"]}
             onAccessToken={(token) => setToken(token)}
           />
         </div>
